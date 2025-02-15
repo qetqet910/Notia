0e78bdc2f0e667c779a480bd47d4197ee0a4f727
5:14 <79036088+qetqet910@users.noreply.github.com>
2025-02-15 07:44:14 +0000

    Input.css 파일 삭제 및 tsconfig.json에서 경로 별칭 추가, vite.config.ts에서 경로 별칭 설정, package.json에 새로운 패키지 추가, README.md 내용 업데이트

--- Amnesia/vite.config.ts
+++ Amnesia/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from "path"
 
 export default defineConfig({
   plugins: [
@@ -35,6 +36,11 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
