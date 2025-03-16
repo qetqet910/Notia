95ad4d583192f2df392165c540cce1e59c3a0283
5:14 <79036088+qetqet910@users.noreply.github.com>
2025-03-16 08:21:12 +0000

    chore: branch test

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {supabse}
+import { supabase } from "@/services/supabase";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Button } from '@/components/ui/button';
