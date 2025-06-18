8fa3d344c8e1e049347144b5ffff73258f01ad20
5:14 <79036088+qetqet910@users.noreply.github.com>
2025-06-18 12:16:48 +0000

    CHORE: ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import { reactHooks } from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ['airbnb-base', 'prettier'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { reactHooks, js },
+    extends: ['airbnb-base', 'prettier'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ]);
